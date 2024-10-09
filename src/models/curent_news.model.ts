5f29b79c30349539de0ed52681f6517e0183a7b3
Mason-bldrj <baterdene.bldrj@gmail.com>
2024-10-09 10:37:47 +0800

    12

--- src/models/curent_news.model.ts
+++ src/models/curent_news.model.ts
@@ -4,4 +4,8 @@ export const Curent_NewsSchema = new Schema({
   imgs: [String],
   title: String,
   description: String,
+  news_type: {
+    type: String,
+    enum: ['TSAG_UY', 'MEDEE_MEDEELEL'],
+  },
 });
