f900827a1afcdf33935d2cd27c16baf1e5ceac20
Батука Баагий <mason-bldrj@Batukas-MacBook-Pro.local>
2024-10-24 11:52:37 +0800

    new

--- src/models/transparency.model.ts
+++ src/models/transparency.model.ts
@@ -1,7 +1,6 @@
 import { Schema } from 'mongoose';
 
 export const Transparency = new Schema({
-  img: String,
   title: String,
-  subTitle: [{ garchig: String, link: String }],
+  file : File   
 });
